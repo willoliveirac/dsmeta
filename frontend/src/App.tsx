4069c02c3d6d17b7c8d239c5ac4e4237e09340d1
Will Oliveira <will.oliveirac@gmail.com>
2022-11-17 03:57:28 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import NotificationButton from "./components/notification-button"
+
+
 
 function App() {
  return(
+  <>
   <h1>ola mundo</h1>
+  <NotificationButton></NotificationButton>
+  </>
  )
 }
 
---
+++ frontend/src/components/notification-button/index.tsx
@@ -0,0 +1,12 @@
+import notIcon from '../../assets/img/notification-icon.svg'
+import './styles.css'
+
+function NotificationButton() {
+    return(
+        <div className="dsmeta-red-btn">
+        <img src={notIcon} alt="Notificar" />
+      </div>
+    )
+   }
+   
+   export default NotificationButton

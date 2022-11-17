1e97a32882689bf5a9239f9416f8c2feec3d2b38
Will Oliveira <will.oliveirac@gmail.com>
2022-11-17 04:26:08 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,24 @@
+import Header from "./components/header"
 import NotificationButton from "./components/notification-button"
+import SalesCard from "./components/salesCard"
 
 
 
 function App() {
- return(
-  <>
-  <h1>ola mundo</h1>
-  <NotificationButton></NotificationButton>
-  </>
- )
+    return (
+        <>
+
+            <Header />
+            <main>
+                <section id="sales">
+                    <div className="dsmeta-container">
+                        <SalesCard />
+                    </div>
+                </section>
+            </main>
+
+        </>
+    )
 }
 
 export default App
